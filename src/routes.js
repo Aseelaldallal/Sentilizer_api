9ab2bafdea6251c60b9a88efdbf4449519f640a3
aseelaldallal <aseelaldallal@gmail.com>
2018-03-15 11:37:18 -0400

    change /sentilize to post

--- src/routes.js
+++ src/routes.js
@@ -5,14 +5,15 @@ export const routes = [
     path: "/",
     handler: function(request, h) {
       return "This is the homepage";
-    } // triggered when you make a request to a specific resource
+    } // handler triggered when you make a request to a specific resource
   },
   {
-    method: "GET",
+    method: "POST",
     path: "/sentilize",
     handler: function(request, h) {
       // h has info/utilities to help manipulate response we'll send back to client
-      return "This route will be changed to a POST route later";
+      const payload = request.payload;
+      return payload.sentence;
     }
   }
 ];
